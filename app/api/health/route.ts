db97926debb2ca4c95b7a5206da540226c18d533
MicroforceBe <olivier@microforce.be>
2026-05-16 18:30:25 +0200

    Create route.ts

---
+++ app/api/health/route.ts
@@ -0,0 +1,16 @@
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    ok: true,
+    timestamp: new Date().toISOString(),
+  });
+}
+
+export async function POST() {
+  return NextResponse.json({
+    ok: true,
+    timestamp: new Date().toISOString(),
+  });
+}
